5c159c9f31705881c45b266bbe3e39d934d9107d
Thales Macêdo <thalesmacedo1@live.com>
2022-01-19 12:00:43 -0300

    1. Abstraction

---
+++ src/CheckingAccount.java
@@ -0,0 +1,15 @@
+public class CheckingAccount {
+    public int branch;
+    public int number;
+    public int balance;
+
+    public void withdraw(){
+
+    }
+    public void deposit(){
+
+    }
+    public void transfer(){
+
+    }
+}
---
+++ src/SavingsAccount.java
@@ -0,0 +1,15 @@
+public class SavingsAccount {
+    public int branch;
+    public int number;
+    public int balance;
+
+    public void withdraw(){
+
+    }
+    public void deposit(){
+
+    }
+    public void transfer(){
+
+    }
+}
